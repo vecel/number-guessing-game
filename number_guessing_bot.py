a868b55f37f3c1393027937be57b7c32d9bb4977
Mateusz Karandys <mateusz.karandys@gmail.com>
2022-06-06 18:26:10 +0200

    Move some functions to number_guessing_bot file, add bots number selection

--- number_guessing_bot.py
+++ number_guessing_bot.py
@@ -1,2 +1,48 @@
-def test():
-    print("Hello, bot!")
+from random import choice
+
+
+def is_number_correct(str_number):
+    if not str_number.isdecimal():          return False
+    if not has_diffrent_digits(str_number): return False
+    if start_with_zero(str_number):         return False
+    if len(str_number) != 4:                return False
+    return True
+
+def start_with_zero(str_number):
+    return str_number[0] == '0'
+
+def has_diffrent_digits(str_number):
+    for num in str_number:
+        if str_number.count(num) > 1: return False
+    return True
+
+def generate_numbers():
+    valid = []
+    for i in range(1000, 10000):
+        if is_number_correct(str(i)): valid.append(str(i))
+    return valid
+
+
+VALID_NUMBERS = generate_numbers()
+
+
+class NumberGuessingBot:
+
+    def __init__(self):
+        self.number = choice(VALID_NUMBERS)
+        self.possibilities = VALID_NUMBERS
+
+    def answer(self, player_guess):
+        """Return points value of guess"""
+        points = 0.0
+        for i in range(len(player_guess)):
+            if self.number[i] == player_guess[i]:           points += 1
+            elif self.number.count(player_guess[i]) == 1:   points += 0.5
+        return points
+
+    def guess():
+        """Ask player about random, but possible, number then read answer and eliminate invalid numbers"""
+        pass
+
+
+        
--- script.py
+++ script.py
@@ -1,20 +1,7 @@
+from number_guessing_bot import is_number_correct, NumberGuessingBot as Bot
+
 MSG = "Type 4-digits number with different digits:"
 WRONG_NUMBER_MSG = "Wrong number, please type again:"
-
-def is_number_correct(str_number):
-    if not str_number.isdecimal():          return False
-    if not has_diffrent_digits(str_number): return False
-    if start_with_zero(str_number):         return False
-    if len(str_number) != 4:                return False
-    return True
-
-def start_with_zero(str_number):
-    return str_number[0] == '0'
-
-def has_diffrent_digits(str_number):
-    for num in str_number:
-        if str_number.count(num) > 1: return False
-    return True
 
 def run():
     print(MSG)
@@ -25,5 +12,6 @@ def run():
         player_number = input()
     
     print('Thanks, your number is ', player_number)
+    bot = Bot()
 
 run()
